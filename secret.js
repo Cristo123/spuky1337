5248789d4caab4e5f8f3b790ecc292382ced26cc
Cristo123 <44101791+Cristo123@users.noreply.github.com>
2018-10-22 00:37:03 +0300

    Update secret.js

--- secret.js
+++ secret.js
@@ -19,9 +19,9 @@ client.on('guildMemberAdd', member => {
 });
 
 client.on('guildMemberAdd', member => {
-    member.guild.channels.get('503674599158972417').setName(`Members: ${member.guild.memberCount}`);
+    member.guild.channels.get('503683011397812234').setName(`Total Users: ${member.guild.memberCount}`);
 });
 
 client.on('guildMemberRemove', member => {
-    member.guild.channels.get('503674599158972417').setName(`Members: ${member.guild.memberCount}`);
+    member.guild.channels.get('503683011397812234').setName(`Total Users: ${member.guild.memberCount}`);
 });
