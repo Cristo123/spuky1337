67526cbd09f602492f8bcde9f7ea218071069028
Cristo123 <44101791+Cristo123@users.noreply.github.com>
2018-12-27 02:23:15 +0200

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,19 +1,2 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-const token = 'NTAzNjY0MTYxNDIzNjIyMTU1.Dq52dw.b84_tWLGgjOOxFAkKIPVf4q_wA0'
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('ready', () => {
-  client.user.setGame('Spooky Welcomer 24/7', 'https://www.twitch.tv/streamerhouse')
-})
-
-client.login('NTAzNjY0MTYxNDIzNjIyMTU1.Dq52dw.b84_tWLGgjOOxFAkKIPVf4q_wA0');
-
-client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find('name', 'general');
-  if (!channel) return;
-  channel.send(`:envelope_with_arrow: [+] ${member} :tada: `);
-});
+const c
